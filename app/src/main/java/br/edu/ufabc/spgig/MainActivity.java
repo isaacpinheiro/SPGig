7e6eca5a2ff5836f677cac0236c7664752e8c52c
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-22 16:26:38 -0300

    Primeiro commit.

---
+++ app/src/main/java/br/edu/ufabc/spgig/MainActivity.java
@@ -0,0 +1,14 @@
+git ipackage br.edu.ufabc.spgig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
