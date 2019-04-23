38e3672a849af33b9f5d909572eaf2ef3296b93c
Isaac Pinheiro <isaacpnhr@gmail.com>
2019-04-23 01:32:06 -0300

    Mudando as cores do tema a adicionando novas atividades.

---
+++ app/src/main/java/br/edu/ufabc/spgig/GigActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ufabc.spgig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gig);
+    }
+}
